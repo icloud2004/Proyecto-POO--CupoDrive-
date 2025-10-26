404aad1f0e7f14c04741d2bc31a210bc18bf1767
Erick Vilela <erickvc813@gmail.com>
2025-10-26 18:21:13 -0500

    nueva clase_ cargar datos

---
+++ Cargar_datos.py
@@ -0,0 +1,11 @@
+import csv
+
+aspirantes = []
+
+class cargar_datos():
+    def cargar(self):
+        with open(r"C:\Users\LENOVO\Documents\GitClonacion\Proyecto-POO--CupoDrive-\BaseDatos.csv",newline="",encoding='utf-8') as data:
+            lector = csv.reader(data,delimiter=";")
+            next(lector)
+            for fila in lector:
+                aspirantes.append(fila)
