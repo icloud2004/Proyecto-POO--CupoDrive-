ef1710e00d226bfef7e985c62f702f940e9bc2b9
icloud2004 <audiman102@gmail.com>
2025-10-26 18:47:01 -0500

    Actualizacion Aspirante

--- Aspirante.py
+++ Aspirante.py
@@ -1,22 +1,23 @@
-class Aspirante:
-    
-    #Constrtuctor
-    def __init__(self, id_aspirante, cedula, nombre, grupo, titulos, estado, vulnerabilidad, fecha_inscripción):
-        self.id_aspirante = id_aspirante
-        self.cedula = cedula
-        self.nombre = nombre
+from persona import Persona
+
+class Aspirante(Persona):
+    def __init__(self, cedula, nombre, puntaje, grupo, titulos, estado, vulnerabilidad, fecha_inscripcion):
+        super().__init__(cedula, nombre)
+        self.puntaje = puntaje
         self.grupo = grupo
         self.titulos = titulos
         self.estado = estado
         self.vulnerabilidad = vulnerabilidad
-        self.fecha_inscripcion = fecha_inscripción
-    
-    def rechazar_cupo(self,cupo):
+        self.fecha_inscripcion = fecha_inscripcion
+
+    def rechazar_cupo(self, cupo):
         pass
-    
-    def aceptar_cupo(self,cupo):
+
+    def aceptar_cupo(self, cupo):
         pass
-    
-    def matricular(self,carrera):
+
+    def matricular(self, carrera):
         pass
-    
+
+    def descripcion(self):
+        return f"Aspirante: {self.nombre}"
