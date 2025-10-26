a9ab981558d96052deb4b77ffc90d588780d1d8d
Leooooxd <erickleodan9@gmail.com>
2025-10-26 18:04:53 -0500

    universidad.py

---
+++ universidad.py
@@ -0,0 +1,38 @@
+class universidad:
+  
+  def __init__ (self, id_universidad, nombre, direccion, telefono, correo, estado, lista_carrera, lista_administradores, lista_periodos, configuracion):
+    self.id_universidad=id_universidad
+    self.nombre=nombre
+    self.direccion=direccion
+    self.telefono=telefono
+    self.correo=correo
+    self.estado=estado
+    self.lista_carrera=lista_carrera
+    self.lista_administradores=lista_administradores
+    self.lista_periodos=lista_periodos
+    self.configuracion=configuracion
+    
+  def agregar_carrera():
+    pass
+    
+  def elminar_carrera():
+    pass
+    
+  def listar_carrera():
+    pass
+    
+  def agregar_administrador():
+    pass
+    
+  def agregar_periodo():
+    pass
+    
+  def listar_periodos():
+    pass
+    
+  def configurar():
+    pass
+  def consultar_estado():
+    pass
+  def generar_reporte():
+    pass
