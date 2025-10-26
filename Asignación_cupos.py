57fa9c85c4167b5aa57bd2e7f4fdc42f80d428f0
ReyesAdrian-11 <jamieadrianreyesbailon@gmail.com>
2025-10-26 17:46:53 -0500

    actualizacion de atributos y metodos de asignacion de cupo

---
+++ Asignación_cupos.py
@@ -0,0 +1,14 @@
+class Asignacion_cupo:
+    def __init__(self, carrera, aspirante, segmento):
+        self.carrera = carrera
+        self.aspirante = aspirante
+        self.segmento = segmento
+
+    def asignar_cupos(self):
+        pass
+
+    def resolver_empates(self):
+        pass
+
+    def liberar_cupos(self):
+        pass
