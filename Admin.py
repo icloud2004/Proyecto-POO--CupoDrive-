637d06830cae777248f0f069b23d2ea1ac2e8cd7
Leooooxd <erickleodan9@gmail.com>
2025-10-26 18:45:15 -0500

    Admin.py

--- Admin.py
+++ Admin.py
@@ -1,4 +1,5 @@
 from Reporte import *
+from persona import Persona
 
 class Administrador:
     def __init__(self, cedula, usuario, contrasena, rol):
@@ -18,3 +19,26 @@ class Administrador:
 
     def modificar_segmento(self):
         pass
+
+
+class Administrador(Persona):
+    def init(self, cedula, nombre, usuario, contrasena, rol):
+        super().init(cedula, nombre)
+        self.usuario = usuario
+        self.contrasena = contrasena
+        self.rol = rol
+
+    def asignar_cupos(self):
+        pass
+
+    def liberar_cupos(self):
+        pass
+
+    def generar_reporte(self):
+        pass
+
+    def modificar_segmento(self):
+        pass
+
+    def descripcion(self):
+        return f"Administrador: {self.nombre}"
