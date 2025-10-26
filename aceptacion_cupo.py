cdc21a72ae9780cc7055fb125c6ec650e2c31b09
icloud2004 <audiman102@gmail.com>
2025-10-26 16:07:26 -0500

    definicion de atributos y metodos en aceptacion_cupo y carrera

---
+++ aceptacion_cupo.py
@@ -0,0 +1,11 @@
+class Aceptacion_cupo:
+    def __init__(self, aspirante, cupo, fecha_aceptacion):
+        self.aspirante = aspirante
+        self.cupo = cupo
+        self.fechaaceptacion = fecha_aceptacion
+    
+    def aceptar(self):
+        pass
+
+    def generar_certificado(self):
+        pass
---
+++ carrera.py
@@ -0,0 +1,11 @@
+class Carrera:
+    def __init__(self, id_carrera, nombre, oferta_cupos, segmento_cupos):
+        self.id_carrera = id_carrera
+        self.nombre = nombre
+        self.oferta_cupos = oferta_cupos
+        self.segmento_cupos = segmento_cupos
+    
+    def agregar_cupo(self,cupo):
+        pass
+    def obtener_cupo(self,segmento):
+        pass
