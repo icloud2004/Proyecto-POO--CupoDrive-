f3bdf666b15f76d1b376eac3c2bdc8cf5e618010
icloud2004 <audiman102@gmail.com>
2025-10-27 18:46:45 -0500

    Actualizacion cargar_datos

--- Cargar_datos.py
+++ Cargar_datos.py
@@ -1,13 +1,31 @@
 import csv
+from Aspirante import Aspirante
 
-aspirantes = []
-
-class cargar_datos():
+class Cargar_datos:
+    def __init__(self, ruta_csv="BaseDatos.csv"):
+        self.ruta_csv = ruta_csv
+        self.aspirantes = []
     def cargar(self):
-        with open(r"C:\Users\LENOVO\Documents\GitClonacion\Proyecto-POO--CupoDrive-\BaseDatos.csv",newline="",encoding='utf-8') as data:
-            lector = csv.reader(data,delimiter=";")
-            next(lector)
+        with open(self.ruta_csv, newline="", encoding="utf-8") as data:
+            lector = csv.reader(data, delimiter=";")
+            next(lector)  # saltar encabezado
+
             for fila in lector:
-                aspirantes.append(fila)
-                print(fila)
-        return aspirantes
+                (
+                    ies_id, ies_nombre, identificacion, nombres, apellidos,
+                    puntaje_postulacion, prioridad, segmento, nombre_carrera,
+                    campus, tipo_cupo, modalidad, nivel, jornada,
+                    acepta_estado, fecha_acepta_cupo
+                ) = fila
+
+                aspirante = Aspirante(
+                    identificacion=identificacion,
+                    nombres=nombres,
+                    apellidos=apellidos,
+                    puntaje=puntaje_postulacion,
+                    carrera=nombre_carrera
+                )
+                self.aspirantes.append(aspirante)
+
+        print(f" Cargados {len(self.aspirantes)} aspirantes desde {self.ruta_csv}.")
+        return self.aspirantes
