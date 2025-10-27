a3ce23865585793923bff11817d3ad3106b81c1d
Leooooxd <erickleodan9@gmail.com>
2025-10-26 21:55:58 -0500

    Create Repositoriocupos.py

---
+++ Repositoriocupos.py
@@ -0,0 +1,9 @@
+class Repositoriocupos:
+    def actualizar_estado_aspirante(self, aspirante, nuevo_estado):
+        pass
+
+    def actualizar_estado_cupo(self, cupo, nuevo_estado):
+        pass
+
+    def registrar_aceptacion(self, aspirante, cupo, fecha):
+        pass
