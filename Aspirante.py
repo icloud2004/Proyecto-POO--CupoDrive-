30d58cab3e7638da252e8ed2b17141f543c53a6e
Leooooxd <erickleodan9@gmail.com>
2025-10-26 21:56:56 -0500

    Aspirante.py
    Added base_datos parameter to constructor and updated methods to save aspirante state.

--- Aspirante.py
+++ Aspirante.py
@@ -1,7 +1,7 @@
 from Persona import Persona
 
 class Aspirante(Persona):
-    def __init__(self, cedula, nombre, puntaje, grupo, titulos, estado, vulnerabilidad, fecha_inscripcion):
+    def __init__(self, cedula, nombre, puntaje, grupo, titulos, estado, vulnerabilidad, fecha_inscripcion, base_datos):
         super().__init__(cedula, nombre)
         self.puntaje = puntaje
         self.grupo = grupo
@@ -9,12 +9,15 @@ class Aspirante(Persona):
         self.estado = estado
         self.vulnerabilidad = vulnerabilidad
         self.fecha_inscripcion = fecha_inscripcion
+        self.base_datos = base_datos  
 
     def rechazar_cupo(self, cupo):
-        pass
+        self.estado = "Rechazado"
+        self.base_datos.guardar_estado_aspirante(self.cedula, self.estado)
 
     def aceptar_cupo(self, cupo):
-        pass
+        self.estado = "Aceptado"
+        self.base_datos.guardar_estado_aspirante(self.cedula, self.estado)
 
     def descripcion(self):
         return f"Aspirante: {self.nombre}"
