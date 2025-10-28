187210f2cec982f0ad0cf53b9240300dce155bce
ReyesAdrian-11 <jamieadrianreyesbailon@gmail.com>
2025-10-27 23:14:36 -0500

    Actualizacion de clase persona

--- Persona.py
+++ Persona.py
@@ -7,3 +7,13 @@ class Persona(ABC):
     @abstractmethod
     def descripcion(self):
         pass
+# Caso de uso
+class Estudiante(Persona):
+    def __init__(self, cedula, nombre, carrera):
+        super().__init__(cedula, nombre)
+        self.carrera = carrera
+    def descripcion(self):
+        return (f"{self.nombre} con cédula {self.cedula} es estudiante de {self.carrera}.")
+    
+estudiante1 = Estudiante("123456784", "Jorge Luis", "Tecnologia de la informacion")
+print(estudiante1.descripcion())
