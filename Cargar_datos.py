33f540204e920b468bfc5c35c8832e99c0618b83
icloud2004 <audiman102@gmail.com>
2025-10-27 19:20:55 -0500

    Actualizacion cargar_datos1

--- Cargar_datos.py
+++ Cargar_datos.py
@@ -5,27 +5,39 @@ class Cargar_datos:
     def __init__(self, ruta_csv="BaseDatos.csv"):
         self.ruta_csv = ruta_csv
         self.aspirantes = []
+
+
     def cargar(self):
-        with open(self.ruta_csv, newline="", encoding="utf-8") as data:
-            lector = csv.reader(data, delimiter=";")
-            next(lector)  # saltar encabezado
+        """Carga los aspirantes desde el archivo CSV (BaseDatos.csv)."""
+        try:
+            with open(self.ruta_csv, newline="", encoding="utf-8") as data:
+                lector = csv.reader(data, delimiter=";")
+                next(lector)  # Saltar encabezado
 
-            for fila in lector:
-                (
-                    ies_id, ies_nombre, identificacion, nombres, apellidos,
-                    puntaje_postulacion, prioridad, segmento, nombre_carrera,
-                    campus, tipo_cupo, modalidad, nivel, jornada,
-                    acepta_estado, fecha_acepta_cupo
-                ) = fila
+                for fila in lector:
+                    (
+                        ies_id, ies_nombre, identificacion, nombres, apellidos,
+                        puntaje_postulacion, prioridad, segmento, nombre_carrera,
+                        campus, tipo_cupo, modalidad, nivel, jornada,
+                        acepta_estado, fecha_acepta_cupo
+                    ) = fila
 
-                aspirante = Aspirante(
-                    identificacion=identificacion,
-                    nombres=nombres,
-                    apellidos=apellidos,
-                    puntaje=puntaje_postulacion,
-                    carrera=nombre_carrera
-                )
-                self.aspirantes.append(aspirante)
+                  
+                    aspirante = Aspirante(
+                        cedula=identificacion,
+                        nombre=f"{nombres} {apellidos}",
+                        puntaje=float(puntaje_postulacion),
+                        grupo=segmento,
+                        titulos="Bachiller",
+                        estado="Postulado",
+                        vulnerabilidad="Alta" if int(segmento) == 1 else "Media",
+                        fecha_inscripcion="2025-01-10"
+                    )
+                    self.aspirantes.append(aspirante)
 
-        print(f" Cargados {len(self.aspirantes)} aspirantes desde {self.ruta_csv}.")
-        return self.aspirantes
+            print(f" Cargados {len(self.aspirantes)} aspirantes desde '{self.ruta_csv}'.")
+            return self.aspirantes
+
+        except FileNotFoundError:
+            print(f" Error: no se encontró el archivo {self.ruta_csv}.")
+            return []
