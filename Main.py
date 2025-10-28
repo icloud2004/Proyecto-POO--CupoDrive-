5e4a8d19653f1ab26a840bb33184897dea6e26e6
icloud2004 <audiman102@gmail.com>
2025-10-28 00:29:54 -0500

    MAIN #1

--- Main.py
+++ Main.py
@@ -1,1 +1,40 @@
+from Cargar_datos import Cargar_datos
+from Carrera import Carrera
+from Asignación_cupos import Asignacion_cupo
+from Admin import Admin
+from Aceptacion_cupo import Aceptacion_cupo
+from Certificado_aceptacion import Certificado_aceptacion
+from Repositoriocupos import RepositorioCupos
+from Segmento import Segmento
+from Universidad import Universidad
+from Periodo_acádemico import Periodo_academico
 
+
+#EL MAIN LO BASAMOS RESPECTO A LA BASE DE DATOS CSV QUE TENEMOS EN NUESTRO REPOSITORIO.
+
+def main():
+    
+    print("  SISTEMA AUTOMATIZADO DE ASIGNACIÓN DE CUPOS (CUPODRIVE)")
+ 
+
+    datos = Cargar_datos("BaseDatos.csv").cargar()
+
+    #Crear universidad y periodo académico
+    uni = Universidad(
+        id_universidad="102",
+        nombre="UNIVERSIDAD LAICA ELOY ALFARO DE MANABÍ",
+        direccion="Manta - Ecuador",
+        telefono="052-600-123",
+        correo="info@uleam.edu.ec",
+        estado="Activa"
+    )
+
+    periodo = Periodo_academico(
+        id_periodo="2025A",
+        nombre="Primer Periodo Académico 2025",
+        fecha_inicio="2025-01-15",
+        estado="Pendiente",
+        universidad=uni
+    )
+    uni.agregar_periodo(periodo)
+    periodo.activar()
