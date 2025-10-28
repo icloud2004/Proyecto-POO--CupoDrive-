99ccc5ad44888e7c9af096399e3573f7537c770e
Leooooxd <erickleodan9@gmail.com>
2025-10-27 23:18:26 -0500

    Update Aspirante.py

--- Aspirante.py
+++ Aspirante.py
@@ -24,3 +24,51 @@ class Aspirante(Persona):
 
     def descripcion(self):
         return f"Aspirante: {self.nombre} ({self.puntaje} puntos, estado: {self.estado})
+        from Persona import Persona
+
+#ejemplos uso
+
+class Cupo:
+    def __init__(self, id_cupo, carrera):
+        self.id_cupo = id_cupo
+        self.carrera = carrera
+
+if __name__ == "__main__":
+    aspirante1 = Aspirante(
+        cedula="1100456789",
+        nombre="José Herrera",
+        puntaje=812,
+        grupo="Mérito Académico",
+        titulos="Bachiller en Ciencias",
+        estado="Postulado",
+        vulnerabilidad="Media",
+        fecha_inscripcion="2025-02-01"
+    )
+
+    aspirante2 = Aspirante(
+        cedula="1100567890",
+        nombre="María López",
+        puntaje=720,
+        grupo="Población General",
+        titulos="Bachiller Técnico",
+        estado="Postulado",
+        vulnerabilidad="Alta",
+        fecha_inscripcion="2025-02-02"
+    )
+
+    
+    cupo_software = Cupo(id_cupo=1, carrera="Ingeniería en Software")
+    cupo_ti = Cupo(id_cupo=2, carrera="Tecnologías de la Información")
+
+    print(" Estado inicial:")
+    print(aspirante1.descripcion())
+    print(aspirante2.descripcion())
+    print()
+
+    aspirante1.aceptar_cupo(cupo_software)
+    aspirante2.rechazar_cupo(cupo_ti)
+    print()
+
+    print(" Estado final:")
+    print(aspirante1.descripcion())
+    print(aspirante2.descripcion())
